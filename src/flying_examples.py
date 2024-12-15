b41e23582d4408b72c17c734bbc929aed5cb1d39
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-12-15 21:20:01 +0530

    [feat] add shm code, yet to test

--- src/flying_examples.py
+++ src/flying_examples.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 from cflib.positioning.motion_commander import MotionCommander
@@ -5,9 +6,11 @@ from cflib.utils import uri_helper
 
 from src.drone import Drone
 
+
 def take_off_simple(mc: MotionCommander):
     time.sleep(3)
     mc.stop()
+
 
 def move_linear_simple(mc: MotionCommander):
     time.sleep(3)
@@ -20,6 +23,7 @@ def move_linear_simple(mc: MotionCommander):
     time.sleep(3)
     mc.stop()
 
+
 def move_square(mc: MotionCommander):
     time.sleep(3)
 
@@ -31,6 +35,7 @@ def move_square(mc: MotionCommander):
 
     time.sleep(1)
     mc.stop()
+
 
 def move_cube(mc: MotionCommander):
     time.sleep(3)
@@ -51,6 +56,7 @@ def move_cube(mc: MotionCommander):
     time.sleep(2)
     mc.stop()
 
+
 def move_cricle(mc: MotionCommander):
     time.sleep(3)
 
@@ -66,12 +72,41 @@ def move_cricle(mc: MotionCommander):
     mc.stop()
 
 
+def oscillate_sinusoidal(
+    mc: MotionCommander, amplitude: float, time_period: float, phi_0: float = 0
+):
+    # TODO: is this correct?
+    omega = 2 * math.pi / time_period  # Angular frequency
+
+    # TODO: is this correct for phase?
+    initial_wait_time = phi_0 / omega
+    if initial_wait_time > 0:
+        time.sleep(initial_wait_time)
+
+    start_time = time.time()
+
+    while True:
+        elapsed_time = time.time() - start_time
+        _x_position = amplitude * math.sin(omega * elapsed_time)
+
+        x_velocity = amplitude * omega * math.cos(omega * elapsed_time)
+
+        # TODO: will this work?
+        if x_velocity >= 0:
+            mc.start_forward(x_velocity)
+        else:
+            mc.start_back(-x_velocity)
+
+        time.sleep(0.1)  # control update rate
+    mc.stop()
+
 
 if __name__ == "__main__":
     uri = uri_helper.uri_from_env(default="radio://0/80/2M/E7E7E7E7E7")
     d = Drone(uri)
-    d.fly(take_off_simple, log=True)
-    d.fly(move_linear_simple, log=True)
-    d.fly(move_square, log=True)
-    d.fly(move_cube, log=True)
-    d.fly(move_cricle, log=True)
+    # d.fly(take_off_simple, log=True)
+    # d.fly(move_linear_simple, log=True)
+    # d.fly(move_square, log=True)
+    # d.fly(move_cube, log=True)
+    # d.fly(move_cricle, log=True)
+    d.fly(oscillate_sinusoidal, log=True)
