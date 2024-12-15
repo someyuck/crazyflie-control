746788446a060153d9a80e2043d5ad74a27a6a0e
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-12-15 19:30:13 +0530

    [refactor] add drone class, fn execution interface, may not be clean

--- src/drone.py
+++ src/drone.py
@@ -1,23 +1,30 @@
-import logging
 import time
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
-# URI to the Crazyflie to connect to
-uri = 'radio://0/80/2M/E7E7E7E7E7'
+
+class Drone:
+    def __init__(self, uri: str = "radio://0/80/2M/E7E7E7E7E7"):
+        self.uri = uri
+        cflib.crtp.init_drivers()
+
+        self.scf: SyncCrazyflie | None = None
+
+    def execute(self, fn):
+        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache="./cache")) as self.scf:
+            fn()
+
 
 def simple_connect():
-
     print("Yeah, I'm connected! :D")
     time.sleep(3)
     print("Now I will disconnect :'(")
 
-if __name__ == '__main__':
-    # Initialize the low-level drivers
-    cflib.crtp.init_drivers()
 
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-
-        simple_connect()
+if __name__ == "__main__":
+    # URI to the Crazyflie to connect to
+    uri = "radio://0/80/2M/E7E7E7E7E7"
+    d = Drone(uri)
+    d.execute(simple_connect)
