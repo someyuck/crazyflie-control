97c06f420c6cee8e84e2f6d4c1c0b94a0cc6c4b2
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-12-15 19:34:10 +0530

    [feat] add sync logging tut code

--- src/drone.py
+++ src/drone.py
@@ -1,14 +1,27 @@
+import logging
 import time
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncLogger import SyncLogger
+
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
 
 class Drone:
     def __init__(self, uri: str = "radio://0/80/2M/E7E7E7E7E7"):
         self.uri = uri
         cflib.crtp.init_drivers()
+
+        lg_stab = LogConfig(name="Stabilizer", period_in_ms=10)
+        lg_stab.add_variable("stabilizer.roll", "float")
+        lg_stab.add_variable("stabilizer.pitch", "float")
+        lg_stab.add_variable("stabilizer.yaw", "float")
 
         self.scf: SyncCrazyflie | None = None
 
@@ -23,8 +36,20 @@ def simple_connect():
     print("Now I will disconnect :'(")
 
 
+def simple_log(scf, logconf):
+    with SyncLogger(scf, logconf) as logger:
+        for log_entry in logger:
+            timestamp = log_entry[0]
+            data = log_entry[1]
+            logconf_name = log_entry[2]
+
+            print("[%d][%s]: %s" % (timestamp, logconf_name, data))
+            break
+
+
 if __name__ == "__main__":
     # URI to the Crazyflie to connect to
     uri = "radio://0/80/2M/E7E7E7E7E7"
     d = Drone(uri)
-    d.execute(simple_connect)
+    # d.execute(simple_connect)
+    d.execute(simple_log)
