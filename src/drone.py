d4d7e64a79ef97021b80bfe9b5103031f2e42043
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-12-15 19:36:48 +0530

    [fix] wasn't passing params to logger fn

--- src/drone.py
+++ src/drone.py
@@ -18,16 +18,16 @@ class Drone:
         self.uri = uri
         cflib.crtp.init_drivers()
 
-        lg_stab = LogConfig(name="Stabilizer", period_in_ms=10)
-        lg_stab.add_variable("stabilizer.roll", "float")
-        lg_stab.add_variable("stabilizer.pitch", "float")
-        lg_stab.add_variable("stabilizer.yaw", "float")
+        self.lg_stab = LogConfig(name="Stabilizer", period_in_ms=10)
+        self.lg_stab.add_variable("stabilizer.roll", "float")
+        self.lg_stab.add_variable("stabilizer.pitch", "float")
+        self.lg_stab.add_variable("stabilizer.yaw", "float")
 
         self.scf: SyncCrazyflie | None = None
 
-    def execute(self, fn):
+    def execute(self, fn, **kwags):
         with SyncCrazyflie(uri, cf=Crazyflie(rw_cache="./cache")) as self.scf:
-            fn()
+            fn(**kwags)
 
 
 def simple_connect():
@@ -36,7 +36,7 @@ def simple_connect():
     print("Now I will disconnect :'(")
 
 
-def simple_log(scf, logconf):
+def simple_log(scf: SyncCrazyflie, logconf: LogConfig):
     with SyncLogger(scf, logconf) as logger:
         for log_entry in logger:
             timestamp = log_entry[0]
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     uri = "radio://0/80/2M/E7E7E7E7E7"
     d = Drone(uri)
     # d.execute(simple_connect)
-    d.execute(simple_log)
+    d.execute(simple_log, d.scf, d.lg_stab)
