5ef5295e82531849154cc75958f3e5c15e1c301e
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2025-01-19 00:30:16 +0530

    [fix] bug in multiple drones example uris

--- src/examples/multiple.py
+++ src/examples/multiple.py
@@ -7,17 +7,19 @@ from src.control.drone import Drone
 import src.examples.flying as examples
 
 URIS = [
-    "radio://0/80/2M/E7E7E7E701",
-    "radio://0/80/2M/E7E7E7E702",
-    "radio://0/80/2M/E7E7E7E703",
+    uri_helper.uri_from_env(default=uri)
+    for uri in [
+        "radio://0/80/2M/E7E7E7E701",
+        "radio://0/80/2M/E7E7E7E702",
+        "radio://0/80/2M/E7E7E7E703",
+    ]
 ]
-
-URIS = [uri_helper.uri_from_env(uri) for uri in URIS]
 
 
 def control_drone(uri: str, id: int, log_file: pathlib.Path):
     d = Drone(uri=uri, id=id, log_file=log_file)
     d.fly(examples.take_off_simple)
+    # d.sync_log_simple()
 
 
 if __name__ == "__main__":
